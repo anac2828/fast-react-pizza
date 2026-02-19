b7733d04d1ec1459899b0010c700c8857bc351c7
anac2828 <anac2828@yahoo.com>
2026-02-19 11:09:55 -0800

    updated vite.config.js base url

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslint()],
-  build: { outDir: 'dist' },
-  base: '/fast-react-pizza',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react(), eslint()],
+    build: { outDir: 'dist' },
+    base: command === 'serve' ? '/' : '/fast-react-pizza',
+  }
 })
